a1ff1786add91642ea6b8733dc62ca5f9fc855b5
McDonaghMichael <G00421944>
2025-12-06 22:24:14 +0000

    Implemented shift key mappings

--- kernel.c
+++ kernel.c
@@ -50,6 +50,8 @@ void print_char(char c, unsigned char color);
 unsigned char k_fetch();
 unsigned char k_getc();
 
+bool shiftEnabled = false;
+
 /**
 Function that returns the length of a string
  */
@@ -104,6 +106,11 @@ unsigned char k_getc(void) {
     unsigned char scancode = k_fetch();
 
 
+    // Checking if the shift button has been released
+    if(scancode == (0x2A + 0x80) || scancode == (0x36 + 0x80)){
+        shiftEnabled = false;
+    }
+
     /**
     Here we check if the keyboard input is released! And if it is released, return
     nothing! As if we didnt do this, it could go straight to checking the value in
@@ -111,6 +118,64 @@ unsigned char k_getc(void) {
      */
     if(scancode & 0x80) {
         return 0;
+    }
+
+    if(scancode == 0x2A || scancode == 0x36){
+        shiftEnabled = true;
+    }
+
+    if(shiftEnabled){
+        switch(scancode) {
+        case 0x02: return '!';
+        case 0x03: return '\"';
+        case 0x04: return '£';
+        case 0x05: return '$';
+        case 0x06: return '%';
+        case 0x07: return '^';
+        case 0x08: return '&';
+        case 0x09: return '*';
+        case 0x0A: return '(';
+        case 0x0B: return ')';
+        case 0x0C: return '_';
+        case 0x0D: return '+';
+        case 0x10: return 'Q';
+        case 0x11: return 'W';
+        case 0x12: return 'E';
+        case 0x13: return 'R';
+        case 0x14: return 'T';
+        case 0x15: return 'Y';
+        case 0x16: return 'U';
+        case 0x17: return 'I';
+        case 0x18: return 'O';
+        case 0x19: return 'P';
+        case 0x1A: return '{';
+        case 0x1B: return '}';
+        case 0x1E: return 'A';
+        case 0x1F: return 'S';
+        case 0x20: return 'D';
+        case 0x21: return 'F';
+        case 0x22: return 'G';
+        case 0x23: return 'H';
+        case 0x24: return 'J';
+        case 0x25: return 'K';
+        case 0x26: return 'L';
+        case 0x27: return ':';
+        case 0x28: return '@';
+        case 0x29: return '¬';
+        case 0x2B: return '|';
+        case 0x2C: return 'Z';
+        case 0x2D: return 'X';
+        case 0x2E: return 'C';
+        case 0x2F: return 'V';
+        case 0x30: return 'B';
+        case 0x31: return 'N';
+        case 0x32: return 'M';
+        case 0x33: return '<';
+        case 0x34: return '>';
+        case 0x35: return '?';
+
+        default: return 0;
+    }
     }
     
     switch(scancode) {
